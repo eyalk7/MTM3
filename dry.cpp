05950a3e7bece4108da1a442be62bf7ac17dff52
Eyal <eyal.kushnir@campus.technion.ac.il>
2019-06-15 22:30:00 +0300

    q2

--- dry.cpp
+++ dry.cpp
@@ -1,4 +1,8 @@
 template<class Iterator, class Predicate>
 int CountPairsByCondition(Iterator first, Iterator last, Predicate pred){
-    // code...
+    // Iterator can use:
+        // ++iterator
+        // *iterator
+        // iterator1 < iterator2
+        // iterator1 == iterator2
 }
