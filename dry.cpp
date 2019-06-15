c0095a505025e1094944b5b51913e7cd0b92201d
Eyal <eyal.kushnir@campus.technion.ac.il>
2019-06-15 23:46:16 +0300

    q2

--- dry.cpp
+++ dry.cpp
@@ -1,3 +1,4 @@
+1)
 template<class Iterator, class Predicate>
 int CountPairsByCondition(Iterator first, Iterator last, Predicate pred){
     // Iterator can use:
@@ -6,3 +7,18 @@ int CountPairsByCondition(Iterator first, Iterator last, Predicate pred){
         // iterator1 < iterator2
         // iterator1 == iterator2
 }
+
+bool isSorted(vector<int> v) {
+    // use the function above
+}
+
+2)
+a.
+    Base::baseMethod
+    Base::method (because not a virtual function)
+    "from Base"
+    A::~A (because dtor is virtual)
+    A::method
+    "from A"
+b. make method virtual?
+c. call from the ctor of A to beseMethod, and than call method
