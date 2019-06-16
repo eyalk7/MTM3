3c8793ff0051064d0cbddfb16857e4c03f808505
Eyal <eyal.kushnir@campus.technion.ac.il>
2019-06-16 20:10:58 +0300

    q2

--- dry.cpp
+++ dry.cpp
@@ -7,9 +7,13 @@ int CountPairsByCondition(Iterator first, Iterator last, Predicate& pred){
         // iterator1 < iterator2
         // iterator1 == iterator2
 
+        pred()
+
     // two loop inside a loop.
     // the outside loop start each time one ahead (so that we don't count the same pair twice)
     // the inside loop starts each time one ahead of the first loop (so that we don't count one object twice as a pair)
+
+
 }
 
 bool isSorted(vector<int> v) {
@@ -18,8 +22,12 @@ bool isSorted(vector<int> v) {
     // use the iterator of the vector container:
         // v.begin, v.end (we need to check that the iterator has all the needed operator
     // create class of "function object" to pass to the function above
-    // check that the first object bigger than the second
-    // each time send two sequential objects
+    //
+    bool operator()(iterator a, iterator b){
+        return (a.index() < b.index() && *a > *b);
+    }
+
+    // checks that returns 0;
 }
 
 2)
@@ -27,8 +35,16 @@ a.
     Base::baseMethod
     Base::method (because not a virtual function)
     "from Base"
-    A::~A (because dtor is virtual)
+    A::~A (because dtor is virtual, so it goes first to the derived dtor)
     A::method
     "from A"
-b. make method virtual?
+    Base::~Base (and after the derived dtor, goes to the base dtor)
+    Base::method
+    "from Base"
+b. make method virtual (?)
 c. call from the ctor of A to beseMethod, and than call method
+
+A() {
+    baseMethod();
+    method();
+}
