9dc6222f1d5dafde8271a049755f2162133d48f9
DaniRabi <danirabi98@gmail.com>
2019-06-26 15:58:40 +0300

    dry.cpp updated

--- dry.cpp
+++ dry.cpp
@@ -1,16 +1,24 @@
 1)
-#include <vector>
-using std::vector;
-
+א.)
+/// Generic function that iterates over a container and
+/// counts the number of pairs that satisfy a given condition
+/// (the order of the pair in the condition does not matter).
+/// \tparam Iterator - Type of Iterator corresponding to the container
+/// \tparam Predicate - The type of condition to check on the pairs
+/// \param first - Iterator pointing to the first item in the container
+/// \param last - Iterator pointing to the last item in the container
+/// \param pred - The condition to check on all the pairs of the container
+/// \return The number of pairs in the container that satisfy the given condition
 template<class Iterator, class Predicate>
-int CountPairsByCondition(cosnt Iterator& first, const Iterator& last, const Predicate& pred){
-    int count=0;
+int CountPairsByCondition(const Iterator& first, const Iterator& last, const Predicate& pred){
+    int count = 0;
 
-    for (Iterator i=first; i<last; ++i){
-        Iterator j=i;
-        ++j;
-        for ( ; j<last; ++j) {
-            if (pred(i,j) && pred(j,i)) { // validate that the order between the elements does not matter
+    for (Iterator i=first; i<last; ++i) {
+        Iterator j = i;
+        ++j;    // j = i + 1 --> the same element will never be a pair on its own
+        for (; j < last; ++j) {
+            // check the condition is satisfied regardless of the pair's order
+            if (pred(i,j) && pred(j,i)) {
                 count++;
             }
         }
@@ -18,59 +26,82 @@ int CountPairsByCondition(cosnt Iterator& first, const Iterator& last, const Pre
     return count;
 }
 
+ב.)
+#include <vector>
+using std::vector;
+
+typedef std::vector<int>::iterator Vector_Iterator;
+
+// Function Object for the predicate in isSorted
+class BiggerThan {
+    int m_bar;
+
+public:
+    BiggerThan(int bar) : m_bar(bar) {}
+
+    // checks that both integers are bigger than bar and not equal to each other
+    bool operator()(const Vector_Iterator& iter1, const Vector_Iterator& iter2) const {
+        return (*iter1 > m_bar && *iter2 > m_bar && *iter1 != *iter2);
+    }
+    void updateBar(int new_bar) {
+        m_bar = new_bar;
+    }
+};
+
+/// Checks if the given integer vector is in ascending order (without equal elements).
+/// \param v - An integer vector
+/// \return True if the given vector is in ascending order, otherwise False.
 bool isSorted(vector<int> v) {
-    typedef std::vector<int>::iterator v_iterator;
+    if (v.empty()) return true; // "empty vector is sorted" is vacuously true
 
-    // predicate object function class
-    class BiggerThan {
-        int bar;
+    BiggerThan condition(v.front() - 1);  // initial condition
 
-    public:
-        BiggerThan(const int c_bar) : bar(c_bar) {
-        }
-        bool operator()(const v_iterator& a, const v_iterator& b) const {
-            return (*a > bar && *b > bar && *a != *b);
-        }
-    };
+    // check that both elements in every consecutive pair is
+    // bigger than the element before and that they aren't equal to each other
+    // <=> check that first element < second element
+    for (Vector_Iterator i = v.begin(); i < v.end() - 1; ++i) {
+        int pair_count = CountPairsByCondition<Vector_Iterator, BiggerThan>(i, i+2, condition);
 
-    int current_bar = v.front() - 1;
-
-    // check that every consecutive pair is bigger than the element before both of them
-    // and that they aren't equal to each other.
-    for (v_iterator i=v.begin(); i<v.end()-1; ++i) {
-        if ( CountPairsByCondition<v_iterator, BiggerThan>(i,i+2,BiggerThan(current_bar)) < 1) {
-            return false;
+        if (pair_count < 1) { // first element >= second element
+            return false;     // vector is not ordered in ascending order
         }
 
-        // else
-        current_bar = *i;
+        condition.updateBar(*i); // update the condition for next pair
     }
 
     return true;
 }
 
 2)
-a.
-    the program calls baseMethod function from a Base pointer, so it search it in Base's public.
-    Base::baseMethod calls method function, which exist in Base (and not virtual), so it calls it.
-    Base::method prints:
+a.)
+    First an object of type A is created and it's being pointed to by a Base pointer.
+    Then, the program calls baseMethod() function from that pointer, so it searches it in Base's public functions.
+    Base::baseMethod() exists so it is called and in its body it calls Base::method() (which is private and not virtual).
+    Base::method() prints:
     "from Base"
-    the we delete base so the d'tor is called. first A::~A (because Base dtor is virtual).
-    A::~A calls method from the class so it goes to A::method, and prints:
+    Then the A object is deleted so the d'tor is called. First, A::~A() is executed (because Base d'tor is virtual).
+    A::~A() calls A::method() in its body, which in turn prints:
     "from A"
-    finally, it goes to Base d'tor - Base::~Base which calls method function from inside the class.
-    so it goes to Base::method, which prints:
+    Finally, the program goes to Base d'tor - Base::~Base() which calls Base::method(), which prints:
     "from Base"
-b.  make Base's method function virtual. that will make the program to check the object real type in run time,
-    and call the relevant function to that type. because base points to an A object, than it will call
-    A::method instead of Base::method. A::method will print "from A" in the first line, instead of "from Base".
-c.  call from the c'tor of A to beseMethod, and than call method
-        A() {
+    Overall:
+    from Base
+    from A
+    from Base
+
+b.)  We can make Base's method() function virtual. That will make the program check the object's real type in runtime,
+    and call the relevant function to that type. The 'base' variable points to an A object, therefore, it will call
+    A::method() instead of Base::method() in the second line in main(). A::method() will then print "from A" in the
+    first line, instead of "from Base".
+
+c.) From A's c'tor call Base::baseMethod(), and then call A::method()
+    In code:
+        A::A() {
             baseMethod();
             method();
         }
-        the c'tor of A will be called when we call new A(). than baseMethod will print:
+        Now, when A's c'tor is called in the first line in main, Base::baseMethod() will print:
         "from Base" (like we explained in a.)
-        and than, method() will call A::method because it calls it from inside A class.
-        than, it will print:
+        and then A::method() will print:
         "from A"
+        as required
